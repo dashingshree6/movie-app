6e7a170674ffe31ab7bde8854c79ce444983a872
Shreyas Naik <83569207+dashingshree6@users.noreply.github.com>
2022-02-09 20:59:55 +0530

    Added routes to App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
+import React from "react";
 import "./styles.scss";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Switch
+} from "react-router-dom";
+import Header from "./components/header/header";
+import Home from "./components/home/home";
+import Footer from "./components/footer/footer";
+import PageNotFound from "./components/pageNotFound/pageNotFound";
+import MovieDetails from "./components/movieDetails/movieDetails";
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <Router>
+        <Header></Header>
+        <Routes>
+          <Route path="/" component={Home} />
+          <Route path="/movie/:imdbID" component={MovieDetails} />
+          <Route component={PageNotFound} />
+        </Routes>
+
+        <Footer />
+      </Router>
     </div>
   );
 }
